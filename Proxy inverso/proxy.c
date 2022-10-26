9e0cabc07d64458eb3e8fde610049c3f747a5fc5
pmayavi <pmayav@eafit.edu.co>
2022-10-26 12:51:29 -0500

    Comentarios

--- Proxy inverso/proxy.c
+++ Proxy inverso/proxy.c
@@ -1,5 +1,5 @@
-// Example code: A simple server side code, which echos back the received message.
-// Handle multiple socket connections with select and fd_set on Linux
+// Codigo adaptado de https://www.geeksforgeeks.org/socket-programming-in-cc-handling-multiple-clients-on-server-without-multi-threading/amp/
+// Por Pablo Maya Villegas
 #include <stdio.h>
 #include <string.h> //strlen
 #include <stdlib.h>
@@ -13,45 +13,49 @@
 #include <strings.h>
 #include <netdb.h>
 
+// Definicion de constantes
 #define TRUE 1
 #define FALSE 0
 
+// Metodo que justifica la causa del error.
 void error(char *msg)
 {
     perror(msg);
     exit(0);
 }
 
+// Metodo main recibe un argumento, el puerto 8080
 int main(int argc, char *argv[])
 {
     int PORT = atoi(argv[1]);
     int opt = TRUE;
-    int master_socket, addrlen, new_socket, client_socket[30],
-        max_clients = 30, activity, i, valread, sd;
-    int max_sd, num_servers = 3, server = 0;
+    // num_servers es la cantidad de servidores que responden las peticiones
+    int master_socket, addrlen, new_socket, num_servers = 3;
+    // max_clients es la cantidad maxima de conecciones distintas que se permiten
+    int server = 0, max_clients = 30, activity, i, valread, sd;
+    int max_sd, client_socket[max_clients];
     struct sockaddr_in address;
 
-    char buffer[2049]; // data buffer of 1K
-    char peticiones[num_servers];
+    char buffer[2049];            // data buffer de 2K
+    char peticiones[num_servers]; // Guarda cual cliente le hizo peticion a un servidor
 
     // set of socket descriptors
     fd_set readfds;
 
-    // initialise all client_socket[] to 0 so not checked
+    // inicializar el array de los sockets guadados como vacios
     for (i = 0; i < max_clients; i++)
     {
         client_socket[i] = 0;
     }
 
-    // create a master socket
+    // se crea el master socket
     if ((master_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // set master socket to allow multiple connections ,
-    // this is just a good habit, it will work without this
+    // Permitirle al master socket multiples conexiones
     if (setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt,
                    sizeof(opt)) < 0)
     {
@@ -59,56 +63,55 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    // type of socket created
+    // tipo de socket creada
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    // bind the socket to localhost port 8888
+    // hacer el bind con el puerto
     if (bind(master_socket, (struct sockaddr *)&address, sizeof(address)) < 0)
     {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
-    printf("Listener on port %d \n", PORT);
+    printf("Escuchando el puerto %d \n", PORT);
 
-    // try to specify maximum of 3 pending connections for the master socket
+    // Especifica que el master socket puede escuchar a 3 clientes en un solo momento
     if (listen(master_socket, 3) < 0)
     {
         perror("listen");
         exit(EXIT_FAILURE);
     }
 
-    // accept the incoming connection
+    // Aceptar las conexiones entrantes
     addrlen = sizeof(address);
     puts("Waiting for connections ...");
 
     while (TRUE)
     {
-        // clear the socket set
+        // Reiniciar el set de sockets
         FD_ZERO(&readfds);
 
-        // add master socket to set
+        // Incluir al master socket en el set de sockets
         FD_SET(master_socket, &readfds);
         max_sd = master_socket;
 
-        // add child sockets to set
+        // Incluir las sockets hijas al set de sockets
         for (i = 0; i < max_clients; i++)
         {
-            // socket descriptor
             sd = client_socket[i];
 
-            // if valid socket descriptor then add to read list
+            // Si es una socket valida
             if (sd > 0)
                 FD_SET(sd, &readfds);
 
-            // highest file descriptor number, need it for the select function
+            // Se guarda el file descriptor mayor para la funcion select mas adelante
             if (sd > max_sd)
                 max_sd = sd;
         }
 
-        // wait for an activity on one of the sockets , timeout is NULL ,
-        // so wait indefinitely
+        // Espera a actividad de alguna socket
+        // Espera indefinidamente/para siempre
         activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
 
         if ((activity < 0) && (errno != EINTR))
@@ -116,8 +119,8 @@ int main(int argc, char *argv[])
             printf("select error");
         }
 
-        // If something happened on the master socket ,
-        // then its an incoming connection
+        // Se detecta un tipo de conexion con la master socket/la ip publica
+        // asi que alguien quiere establecer conexion
         if (FD_ISSET(master_socket, &readfds))
         {
             if ((new_socket = accept(master_socket,
@@ -127,19 +130,20 @@ int main(int argc, char *argv[])
                 exit(EXIT_FAILURE);
             }
 
-            // inform user of socket number - used in send and receive commands
-            printf("New connection , socket fd is %d , ip is : %s , port : %d\n ",
+            // Se guarda con quien se establecio la conexion y mas datos
+            printf("Nueva conexion, socketfd: %d , ip: %s , puerto: %d\n",
                    new_socket, inet_ntoa(address.sin_addr), ntohs(address.sin_port));
 
-            // add new socket to array of sockets
+            // Añade la nueva socket al array de sockets disponibles
             for (i = 0; i < max_clients; i++)
             {
-                // if position is empty
+                // Si no hay una socket en la posicion
                 if (client_socket[i] == 0)
                 {
                     client_socket[i] = new_socket;
-                    printf("Adding to list of sockets as %d\n", i);
+                    printf("Guardando la socket como numero %d\n", i);
 
+                    // Las n primeras conexiones, son los servidores
                     if (i < num_servers)
                     {
                         char num[35];
@@ -151,52 +155,51 @@ int main(int argc, char *argv[])
             }
         }
 
-        // else its some IO operation on some other socket
+        // Si no fue con la master socket, es comunicacion con una socket existente
         for (i = 0; i < max_clients; i++)
         {
             sd = client_socket[i];
-
             if (FD_ISSET(sd, &readfds))
             {
-                // Check if it was for closing , and also read the
-                // incoming message
+                // Mira si fue el cierre de conexion
+                // read lee el mensaje que llego y lo guarda en buffer
                 if ((valread = read(sd, buffer, 2048)) == 0)
                 {
-                    // Somebody disconnected , get his details and print
+                    // Se desconecto, se imprime los detalles de quien fue
                     getpeername(sd, (struct sockaddr *)&address,
                                 (socklen_t *)&addrlen);
-                    printf("Host disconnected , ip %s , port %d \n",
+                    printf("Host desconectado, ip: %s , puerto: %d \n",
                            inet_ntoa(address.sin_addr), ntohs(address.sin_port));
-
-                    // Close the socket and mark as 0 in list for reuse
+                    // Cerrar la socket y dejar disponible el espacio en el array de sockets
                     close(sd);
                     client_socket[i] = 0;
                 }
 
-                // Echo back the message that came in
-                else
+                else // LLego un mensaje o peticion
                 {
-                    // set the string terminating NULL byte on the end
-                    // of the data read
+                    // Poner el fin de la string del mensaje
                     buffer[valread] = '\0';
+                    // Si su numero de socket es mayor al numero de servidores, es un cliente
                     if (i > num_servers - 1)
                     {
+                        // Se envia la peticion a un servidor
                         send(client_socket[server], buffer, strlen(buffer), 0);
-                        peticiones[server] = i;
+                        peticiones[server] = i; // Se guarda cual cliente hizo la solicitud a ese servidor
+                        // Cicla al siguiente servidor de forma Round Robin
                         if (server < num_servers - 1)
                             server = server + 1;
                         else
                             server = 0;
                     }
-                    else if (client_socket[peticiones[i]] != 0)
+                    else if (client_socket[peticiones[i]] != 0) // Es una respuesta de un servidor
                     {
-                        sd = client_socket[peticiones[i]];
+                        sd = client_socket[peticiones[i]]; // Se le entrega al socket que le hizo la peticion
                         send(sd, buffer, strlen(buffer), 0);
                         getpeername(sd, (struct sockaddr *)&address,
                                     (socklen_t *)&addrlen);
-                        printf("Host disconnected , ip %s , port %d \n",
+                        printf("Host desconectado, ip: %s , puerto: %d \n",
                                inet_ntoa(address.sin_addr), ntohs(address.sin_port));
-                        // Close the socket and mark as 0 in list for reuse
+                        // Se cierra la conexion con el cliente quien ya recibio su respuesta
                         close(sd);
                         client_socket[peticiones[i]] = 0;
                     }
--- Proxy inverso/server.c
+++ Proxy inverso/server.c
@@ -1,3 +1,5 @@
+// Codigo adaptado de https://www.cs.rpi.edu/~moorthy/Courses/os98/Pgms/socket.html
+// Por Pablo Maya Villegas
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,12 +10,15 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
+// Metodo que justifica la causa del error.
 void error(char *msg)
 {
     perror(msg);
     exit(0);
 }
 
+// Metodo main recibe dos argumentos,
+// la ip publica o privada del servidor proxy y el puerto 8080
 int main(int argc, char *argv[])
 {
     int sockfd, portno, n;
@@ -27,12 +32,12 @@ int main(int argc, char *argv[])
         fprintf(stderr, "usage %s hostname port\n", argv[0]);
         exit(0);
     }
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    portno = atoi(argv[2]);                   // El puerto
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); // Se abre la socket con el servidor proxy
     if (sockfd < 0)
         error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL)
+    server = gethostbyname(argv[1]); // Se establece la conexion con la ip
+    if (server == NULL)              // No se encontro el host con esa ip
     {
         fprintf(stderr, "ERROR, no such host\n");
         exit(0);
@@ -49,13 +54,14 @@ int main(int argc, char *argv[])
     while (1)
     {
         bzero(buffer, 256);
-        n = read(sockfd, buffer, 255);
+        n = read(sockfd, buffer, 255); // Lee la peticion que le envie el servidor proxy
         if (n < 0)
             error("ERROR reading from socket");
         printf("%s", buffer);
 
-        if (buffer[0] != 0)
+        if (buffer[0] != 0) // Si le llega una peticion valida
         {
+            // Envia una respuesta HTTP la cual le entrega al cliente un html, estos deben tener menos de 2048 caracteres
             char message[2048] = "HTTP/1.0 200 OK\r\nHost: http://54.147.55.156\r\nContent-type: text/html\r\n\r\n<html><head><meta charset=\"utf-8\"><title>Proxy Inverso</title></head><body><h2>Hola desde el servidor 1</h2><img src=\"https://pioneeroptimist.com/wp-content/uploads/2021/03/among-us-6008615_1920-838x900.png\"></body></html>\r\n";
             n = write(sockfd, message, strlen(message));
         }
