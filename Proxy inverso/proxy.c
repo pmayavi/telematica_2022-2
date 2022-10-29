23da12c1c37b4405fffb3c7f757d05cb58d1ef1c
pmayavi <pmayav@eafit.edu.co>
2022-10-29 15:02:29 -0500

    Implementacion de Cache adaptativo

--- Proxy inverso/proxy.c
+++ Proxy inverso/proxy.c
@@ -28,22 +28,30 @@ void error(char *msg)
 // Metodo main recibe un argumento, el puerto 8080
 int main(int argc, char *argv[])
 {
-    float TTL = (float)atof(argv[1]);
-    int PORT = atoi(argv[2]);
+    int PORT = atoi(argv[1]);
     FILE *log;
 
     int opt = TRUE;
     // num_servers es la cantidad de servidores que responden las peticiones
-    int master_socket, addrlen, new_socket, num_servers = 3;
+    int num_servers = 3;
     // max_clients es la cantidad maxima de conecciones distintas que se permiten
-    int server = 0, max_clients = 30, activity, i, valread, sd;
-    int max_sd, client_socket[max_clients];
-    struct sockaddr_in address;
+    int max_clients = 30;
+    // cache_size es el numero maximo de archivos que se guardan en cache
+    int cache_size = 5;
 
+    int master_socket, addrlen, new_socket, activity;
+    int server = 0, i, j, valread, cache_current = 0;
+    int client_socket[max_clients], sd, max_sd;
+    // Cada elemento del cache esta compuesto de 3 cosas: cual es, que es, y la ultima vez que fue usado
+    char cache[cache_size][2048], cachename[cache_size];
+    double TTL, cacheTTL[cache_size];
+    // Usados para el log con tiempo y para el TTL de cada elemento en cache
+    struct timeval tv;
     time_t rawtime;
     struct tm *timeinfo;
+    struct sockaddr_in address;
 
-    char buffer[2049];            // data buffer de 2K
+    char buffer[2048];            // data buffer de 2K
     char peticiones[num_servers]; // Guarda cual cliente le hizo peticion a un servidor
 
     // set of socket descriptors
@@ -93,7 +101,7 @@ int main(int argc, char *argv[])
     // Aceptar las conexiones entrantes
     addrlen = sizeof(address);
     puts("Esperando a los servidores...\n");
-
+    // Loop infinito para mantener el servidor funcionando indefinidamente
     while (TRUE)
     {
         // Reiniciar el set de sockets
@@ -108,11 +116,9 @@ int main(int argc, char *argv[])
         for (i = 0; i < max_clients; i++)
         {
             sd = client_socket[i];
-
             // Si es una socket valida
             if (sd > 0)
                 FD_SET(sd, &readfds);
-
             // Se guarda el file descriptor mayor para la funcion select mas adelante
             if (sd > max_sd)
                 max_sd = sd;
@@ -121,7 +127,6 @@ int main(int argc, char *argv[])
         // Espera a actividad de alguna socket
         // Espera indefinidamente/para siempre
         activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
-
         if ((activity < 0) && (errno != EINTR))
         {
             printf("select error");
@@ -140,8 +145,8 @@ int main(int argc, char *argv[])
             // Se guarda con quien se establecio la conexion y mas datos
             time(&rawtime);
             timeinfo = localtime(&rawtime);
-            printf("%sNueva conexion %s\n\n", asctime(timeinfo), inet_ntoa(address.sin_addr));
-            fprintf(log, "%sNueva conexion %s", asctime(timeinfo), inet_ntoa(address.sin_addr));
+            printf("%sNueva conexion %s\n", asctime(timeinfo), inet_ntoa(address.sin_addr));
+            fprintf(log, "%sNueva conexion %s\n", asctime(timeinfo), inet_ntoa(address.sin_addr));
 
             // Añade la nueva socket al array de sockets disponibles
             for (i = 0; i < max_clients; i++)
@@ -151,9 +156,11 @@ int main(int argc, char *argv[])
                 {
                     client_socket[i] = new_socket;
                     printf("Guardando la socket como numero %d\n\n", i);
+                    fprintf(log, "Guardando la socket como numero %d\n\n", i);
                     // Las n primeras conexiones, son los servidores
                     if (i < num_servers)
                     {
+                        // Se le envia un mensaje al servidor para confirmarle que es un servidor
                         char num[35];
                         snprintf(num, 35, "# Asignado como server %d\n", i);
                         send(new_socket, num, strlen(num), 0);
@@ -162,7 +169,7 @@ int main(int argc, char *argv[])
                 }
             }
         }
-        fclose(log);
+        fclose(log); // El archivo log debe cerrarse y abrirse para que se mantenga actualizando
         // Si no fue con la master socket, es comunicacion con una socket existente
         for (i = 0; i < max_clients; i++)
         {
@@ -197,34 +204,102 @@ int main(int argc, char *argv[])
                                     (socklen_t *)&addrlen);
                         time(&rawtime);
                         timeinfo = localtime(&rawtime);
-                        printf("%sPeticion de %s: %s\n\n", asctime(timeinfo), inet_ntoa(address.sin_addr), buffer);
-                        fprintf(log, "%sPeticion de %s: %s\n\n", asctime(timeinfo), inet_ntoa(address.sin_addr), buffer);
+                        printf("%sPeticion de %s:\n%s\n\n", asctime(timeinfo), inet_ntoa(address.sin_addr), buffer);
+                        fprintf(log, "%sPeticion de %s:\n%s\n\n", asctime(timeinfo), inet_ntoa(address.sin_addr), buffer);
 
-                        // Se envia la peticion a un servidor
-                        send(client_socket[server], buffer, strlen(buffer), 0);
-                        peticiones[server] = i; // Se guarda cual cliente hizo la solicitud a ese servidor
-                        // Cicla al siguiente servidor de forma Round Robin
-                        if (server < num_servers - 1)
-                            server = server + 1;
-                        else
-                            server = 0;
+                        int nocache = TRUE;
+                        for (j = 0; j < cache_size; j++) // Se busca en el cache por la peticion /p
+                        {
+                            if (cachename[j] == buffer[5]) // Si encuentra que en el cahce esta esa peticion
+                            {
+                                // Se le envia la peticion desde cache al cliente y se cierra la conexion
+                                send(sd, cache[j], strlen(cache[j]), 0);
+                                time(&rawtime);
+                                timeinfo = localtime(&rawtime);
+                                printf("%sRespuesta desde cache a %s:\n%s\n", asctime(timeinfo), inet_ntoa(address.sin_addr), cache[j]);
+                                fprintf(log, "%sRespuesta desde cache a %s:\n%s\n", asctime(timeinfo), inet_ntoa(address.sin_addr), cache[j]);
+                                gettimeofday(&tv, NULL);
+                                TTL = (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000;
+                                cacheTTL[j] = TTL; // El TTL de esa peticion se refresca
+                                printf("Host desconectado: %s\n\n", inet_ntoa(address.sin_addr));
+                                fprintf(log, "Host desconectado: %s\n\n", inet_ntoa(address.sin_addr));
+                                // Se cierra la conexion con el cliente quien ya recibio su respuesta
+                                close(sd);
+                                client_socket[i] = 0;
+                                nocache = FALSE;
+                                break;
+                            }
+                        }
+
+                        if (nocache) // Solo hace la peticion a los servidores si no se encontro en el cache
+                        {
+                            // Se envia la peticion a un servidor
+                            send(client_socket[server], buffer, strlen(buffer), 0);
+                            peticiones[server] = i; // Se guarda cual cliente hizo la solicitud a ese servidor
+                            // Cicla al siguiente servidor de forma Round Robin
+                            if (server < num_servers - 1)
+                                server = server + 1;
+                            else
+                                server = 0;
+                        }
                     }
                     else if (peticiones[i] != 0) // Es una respuesta de un servidor
                     {
+                        if (cache_current < cache_size) // El cache esta vacio, sucede al inicio del servidor
+                        {
+                            // Guarda el nombre de la peticion
+                            cachename[cache_current] = buffer[strlen(buffer) - 1];
+                            buffer[strlen(buffer) - 1] = '\0';
+                            // Guarda el cache
+                            strcpy(cache[cache_current], buffer);
+                            gettimeofday(&tv, NULL);
+                            TTL = (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000;
+                            // Guarda el TTL del cache
+                            cacheTTL[cache_current] = TTL;
+                            printf("Guardado en el cache %c\n", cachename[cache_current]);
+                            fprintf(log, "Guardado en el cache %c\n", cachename[cache_current]);
+                            cache_current = cache_current + 1;
+                        }
+                        else // El cache esta lleno
+                        {
+                            double temp = cacheTTL[0];
+                            for (j = 1; j < cache_size; j++)
+                            {
+                                // Busca el cache que ha pasado mas tiempo sin ser usado
+                                if (cacheTTL[j] < temp)
+                                    temp = cacheTTL[j];
+                            }
+                            for (j = 0; j < cache_size; j++)
+                            {
+                                if (cacheTTL[j] == temp)
+                                {
+                                    // Sobre escribe el cahce que paso mas tiempo sin ser usado
+                                    // Guarda el nombre de la peticion
+                                    cachename[j] = buffer[strlen(buffer) - 1];
+                                    buffer[strlen(buffer) - 1] = '\0';
+                                    // Guarda el cache
+                                    strcpy(cache[j], buffer);
+                                    gettimeofday(&tv, NULL);
+                                    TTL = (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000;
+                                    // Guarda el TTL del cache
+                                    cacheTTL[j] = TTL;
+                                    printf("Sobreguardado en el cache %c\n", cachename[j]);
+                                    fprintf(log, "sobreguardado en el cache %c\n", cachename[j]);
+                                }
+                            }
+                        }
                         sd = client_socket[peticiones[i]]; // Se le entrega al socket que le hizo la peticion
 
                         getpeername(sd, (struct sockaddr *)&address,
                                     (socklen_t *)&addrlen);
                         time(&rawtime);
                         timeinfo = localtime(&rawtime);
-                        printf("%sRespuesta a %s: %s\n\n", asctime(timeinfo), inet_ntoa(address.sin_addr), buffer);
-                        fprintf(log, "%sRespuesta a %s: %s\n\n", asctime(timeinfo), inet_ntoa(address.sin_addr), buffer);
+                        printf("%sRespuesta a %s:\n%s\n", asctime(timeinfo), inet_ntoa(address.sin_addr), buffer);
+                        fprintf(log, "%sRespuesta a %s:\n%s\n", asctime(timeinfo), inet_ntoa(address.sin_addr), buffer);
 
                         send(sd, buffer, strlen(buffer), 0);
-                        time(&rawtime);
-                        timeinfo = localtime(&rawtime);
-                        printf("%sHost desconectado: %s\n\n", asctime(timeinfo), inet_ntoa(address.sin_addr));
-                        fprintf(log, "%sHost desconectado: %s\n\n", asctime(timeinfo), inet_ntoa(address.sin_addr));
+                        printf("Host desconectado: %s\n\n", inet_ntoa(address.sin_addr));
+                        fprintf(log, "Host desconectado: %s\n\n", inet_ntoa(address.sin_addr));
                         // Se cierra la conexion con el cliente quien ya recibio su respuesta
                         close(sd);
                         client_socket[peticiones[i]] = 0;
--- Proxy inverso/server.c
+++ Proxy inverso/server.c
@@ -25,13 +25,38 @@ int main(int argc, char *argv[])
 
     struct sockaddr_in serv_addr;
     struct hostent *server;
-
     char buffer[256];
+    char message[2048];
     if (argc < 3)
     {
         fprintf(stderr, "usage %s hostname port\n", argv[0]);
         exit(0);
     }
+    // Cabeza del HTML
+    char file[1024] = "HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n<html><head><meta charset=\"utf-8\"><title>Proxy Inverso</title><link rel=\"icon\" type=\"image/x-icon\" href=\"https://uploads-ssl.webflow.com/62cbec5fe81984e0aac7b23d/62dda486b127ce0a51debf91_6213a3f8e713f6292c8a9526_Prepera%20logo%202.png\"></head><body><h2>Hola desde el servidor 1</h2>";
+    char url[128]; // URL construido con la ip elastica para tener botones funcionales
+    sprintf(url, "http://%s:%s", argv[1], argv[2]);
+    char noimg[2048]; // La pagina inicial tiene botones para ir a las otras paginas
+    sprintf(noimg, "<a href=\"%s/1\" class=\"button\">1</a> <a href=\"%s/2\" class=\"button\">2</a> <a href=\"%s/3\" class=\"button\">3</a> <a href=\"%s/4\" class=\"button\">4</a> <a href=\"%s/5\" class=\"button\">5</a> <a href=\"%s/6\" class=\"button\">6</a> <a href=\"%s/7\" class=\"button\">7</a> <a href=\"%s/8\" class=\"button\">8</a> <a href=\"%s/9\" class=\"button\">9</a> </body></html>\r\n", url, url, url, url, url, url, url, url, url);
+    char img1[1024]; // Cada pagina tiene una imagen distinta
+    sprintf(img1, "<img src=\"https://c.tenor.com/GvP7L0FwFyMAAAAC/dancing-number-dancing-letter.gif\" width=\"250\" height=\"250\"> <a href=\"%s\" class=\"button\">Regresar</a></body></html>\r\n", url);
+    char img2[1024]; //  y un boton para regresar a la principal
+    sprintf(img2, "<img src=\"https://media.tenor.com/J-MYuLM6fXUAAAAi/dancing-number-dancing-letter.gif\" width=\"250\" height=\"250\"> <a href=\"%s\" class=\"button\">Regresar</a></body></html>\r\n", url);
+    char img3[1024];
+    sprintf(img3, "<img src=\"https://media.tenor.com/o96-hgLmCQ0AAAAC/dancing-number-dancing-letter.gif\" width=\"250\" height=\"250\"> <a href=\"%s\" class=\"button\">Regresar</a></body></html>\r\n", url);
+    char img4[1024];
+    sprintf(img4, "<img src=\"https://i.pinimg.com/originals/e5/36/86/e5368693123ea4615e05e11c93f87ce3.gif\" width=\"250\" height=\"250\"> <a href=\"%s\" class=\"button\">Regresar</a></body></html>\r\n", url);
+    char img5[1024];
+    sprintf(img5, "<img src=\"https://media.tenor.com/JD-y9a5fHBsAAAAi/dancing-number-dancing-letter.gif\" width=\"250\" height=\"250\"> <a href=\"%s\" class=\"button\">Regresar</a></body></html>\r\n", url);
+    char img6[1024];
+    sprintf(img6, "<img src=\"https://media.tenor.com/jaAZ3VgcobkAAAAC/dancing-six-dancing-number.gif\" width=\"250\" height=\"250\"> <a href=\"%s\" class=\"button\">Regresar</a></body></html>\r\n", url);
+    char img7[1024];
+    sprintf(img7, "<img src=\"https://media.tenor.com/6-3dxjy6qSMAAAAi/dancing-number-dancing-letter.gif\" width=\"250\" height=\"250\"> <a href=\"%s\" class=\"button\">Regresar</a></body></html>\r\n", url);
+    char img8[1024];
+    sprintf(img8, "<img src=\"https://media.tenor.com/d3HlvsnflX0AAAAC/dancing-number-dancing-letter.gif\" width=\"250\" height=\"250\"> <a href=\"%s\" class=\"button\">Regresar</a></body></html>\r\n", url);
+    char img9[1024];
+    sprintf(img9, "<img src=\"https://media.tenor.com/i0x1-y8QubAAAAAi/dancing-number-dancing-letter.gif\" width=\"250\" height=\"250\"> <a href=\"%s\" class=\"button\">Regresar</a></body></html>\r\n", url);
+
     portno = atoi(argv[2]);                   // El puerto
     sockfd = socket(AF_INET, SOCK_STREAM, 0); // Se abre la socket con el servidor proxy
     if (sockfd < 0)
@@ -59,10 +84,45 @@ int main(int argc, char *argv[])
             error("ERROR reading from socket");
         printf("%s", buffer);
 
-        if (buffer[0] != 0 && buffer[0] != '#') // Si le llega una peticion valida
+        if (buffer[4] == '/') // Si le llega una peticion valida
         {
-            // Envia una respuesta HTTP la cual le entrega al cliente un html, estos deben tener menos de 2048 caracteres
-            char message[2048] = "HTTP/1.0 200 OK\r\nHost: http://54.147.55.156\r\nContent-type: text/html\r\n\r\n<html><head><meta charset=\"utf-8\"><title>Proxy Inverso</title></head><body><h2>Hola desde el servidor 1</h2><img src=\"https://pioneeroptimist.com/wp-content/uploads/2021/03/among-us-6008615_1920-838x900.png\"></body></html>\r\n";
+            strcpy(message, file);
+            switch (buffer[5]) // La petcion es solo el caracter que sigue del /
+            {
+            case '1':
+                strcat(message, img1);
+                break;
+            case '2':
+                strcat(message, img2);
+                break;
+            case '3':
+                strcat(message, img3);
+                break;
+            case '4':
+                strcat(message, img4);
+                break;
+            case '5':
+                strcat(message, img5);
+                break;
+            case '6':
+                strcat(message, img6);
+                break;
+            case '7':
+                strcat(message, img7);
+                break;
+            case '8':
+                strcat(message, img8);
+                break;
+            case '9':
+                strcat(message, img9);
+                break;
+            default:
+                strcat(message, noimg);
+            }
+            char tmpstr[2]; // Le envia al proxy cual es la peticion para su uso en cache
+            tmpstr[0] = buffer[5];
+            tmpstr[1] = 0;
+            strcat(message, tmpstr);
             n = write(sockfd, message, strlen(message));
         }
     }
